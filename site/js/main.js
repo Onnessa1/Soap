319babe687aa673955b6b9fb70aeffb475a15340
chikchicly <chikchicly@gmail.com>
2025-01-10 12:30:49 +0700

    f

--- site/js/main.js
+++ site/js/main.js
@@ -1,6 +1,6 @@
-console.log('It\'s main script' );
 
 document.querySelector('#button').addEventListener('click',CallWatsApp);
 function CallWatsApp(){
     window.location.href=`https://wa.clck.bar/77477064857`;
 }
+
