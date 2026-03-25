bda0c90de5e7f474e64c64c57b9a60681f05d6b2
Amir <chikchicly@gmail.com>
2026-03-25 11:47:04 +0700

    oK

--- site/js/menu.js
+++ site/js/menu.js
@@ -1,15 +1,28 @@
-document.querySelector('#menu').addEventListener('click',()=>{
-    let itSmenu="<h3><a href='#catalog'>Каталог</a></h3> <h3><a href='#reviews'>Отзовы</a></h3> <h3><a href='#contacts'>Контакты</a></h3><h3><a href='tel:+77477064857'>+7 747 706 4857</a></h3>";
-    let okCreate=document.createElement('div');
-    okCreate.classList.add('openMenu');
-    okCreate.innerHTML=`${itSmenu}`;
-    document.querySelector('main').prepend(okCreate);
-    closeMenu();
-});
+document.querySelector('#menu').addEventListener('click', () => {
+    // Если меню уже есть, ничего не делаем
+    if(document.querySelector('.openMenu')) return;
 
-function closeMenu(){
-    document.querySelector('.openMenu').addEventListener('click',()=>{
-        document.querySelector('.openMenu').remove();
+    const itSmenu = `
+        <h3><a href='#catalog'>Каталог</a></h3>
+        <h3><a href='#reviews'>Отзовы</a></h3>
+        <h3><a href='#contacts'>Контакты</a></h3>
+        <h3><a href='tel:+77477064857'>+7 747 706 4857</a></h3>
+    `;
+
+    const okCreate = document.createElement('div');
+    okCreate.classList.add('openMenu');
+    okCreate.innerHTML = itSmenu;
+    document.body.append(okCreate);
+
+    // Анимируем появление
+    requestAnimationFrame(() => {
+        okCreate.classList.add('show');
     });
-}
-// closeMenu();
+
+    // Закрытие при клике на меню
+    okCreate.addEventListener('click', () => {
+        okCreate.classList.remove('show');
+        // После окончания анимации удаляем элемент
+        okCreate.addEventListener('transitionend', () => okCreate.remove(), { once: true });
+    });
+});
